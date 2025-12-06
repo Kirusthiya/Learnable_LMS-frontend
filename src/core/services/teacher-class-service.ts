fcff1ebd4f22c0a35629112cfe32bddd0119e586
Kirusthiya <sureshkirusthiya@gmail.com>
2025-12-06 18:25:08 +0530

    Class create and repo create

---
+++ src/core/services/teacher-class-service.ts
@@ -0,0 +1,63 @@
+import { Injectable, inject, signal, WritableSignal, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { ClassDto } from '../../types/Notification';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherClassService {
+
+  private http = inject(HttpClient);
+  private ngZone = inject(NgZone);
+  private baseUrl = environment.apiUrl;
+
+  public teacherClasses: WritableSignal<ClassDto[]> = signal([]);
+  public selectedClassId: WritableSignal<string | null> = signal(null);
+  public loading: WritableSignal<boolean> = signal(false);
+
+public loadClasses(currentTeacherId: string): void {
+  setTimeout(() => this.loading.set(true));
+
+  this.http.get<ClassDto[]>(`${this.baseUrl}Class/all`).subscribe({
+    next: (res) => {
+      this.ngZone.run(() => {
+        this.teacherClasses.set(res || []);
+        if (res?.length > 0) {
+          this.selectedClassId.set(res[0].classId);
+        }
+        setTimeout(() => this.loading.set(false));
+      });
+    },
+    error: (err) => {
+      console.error("Class not found:", err);
+      this.ngZone.run(() => setTimeout(() => this.loading.set(false)));
+    }
+  });
+}
+  public selectClass(classId: string): void {
+    this.selectedClassId.set(classId);
+  }
+
+  public addClass(name: string, joinName: string, desc: string, teacherId: string) {
+    const payload = {
+      classDto: {
+        className: name,
+        classJoinName: joinName,
+        description: desc,
+        teacherId: teacherId, 
+        status: "Active"
+      }
+    };
+    this.http.post(`${this.baseUrl}Class/create`, payload)
+      .subscribe({
+        next: (res: any) => {
+          console.log("Class created:", res);
+          this.loadClasses(teacherId);
+        },
+        error: (err) => {
+          console.error("Create class failed:", err);
+        }
+      });
+  }
+}
--- src/features/dashboad/home/dashboard-home.ts
+++ src/features/dashboad/home/dashboard-home.ts
@@ -1,4 +1,52 @@
-import { Component, computed, inject, signal } from '@angular/core';
+// import { Component, computed, inject, signal } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { AccountService } from '../../../core/services/accountservices';
+// import { Class } from '../../../types/user';
+// import { Repository } from "../../repository/repository";
+// import { Assets } from '../../assets/assets';
+// import { KeyboardNav } from '../../../core/directives/accessibility/keyboard-nav';
+// import { Speak } from "../../../core/directives/accessibility/speak";
+// import { Search } from '../../search/search';
+
+// @Component({
+//     selector: 'app-dashboard-home',
+//     standalone: true,
+//     imports: [CommonModule, Repository, Assets, KeyboardNav, Speak, Search],
+//     templateUrl: './dashboard-home.html',
+// })
+// export class DashboardHome {
+//     private accountService = inject(AccountService);
+
+//     selectedClassId = signal<string | null>(null);
+//     selectedRepositoryId = signal<string | null>(null);
+
+//     studentClasses = computed<Class[]>(() => {
+//         const user = this.accountService.currentUser();
+//         return user?.student?.classes || [];
+//     });
+
+//     // When class is clicked
+//     viewClass(id: string) {
+//         this.selectedClassId.set(id);
+//     }
+
+//     // Repository clicked
+//     openRepository(repoId: string) {
+//         this.selectedRepositoryId.set(repoId);
+//     }
+
+//     // Back from assets → go to repository list
+//     backFromAssets() {
+//         this.selectedRepositoryId.set(null);
+//     }
+
+//     // Back from repositories → go to class list
+//     backToDashboard() {
+//         this.selectedClassId.set(null);
+//     }
+// }
+
+import { Component, computed, inject, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountService } from '../../../core/services/accountservices';
 import { Class } from '../../../types/user';
@@ -7,41 +55,47 @@ import { Assets } from '../../assets/assets';
 import { KeyboardNav } from '../../../core/directives/accessibility/keyboard-nav';
 import { Speak } from "../../../core/directives/accessibility/speak";
 import { Search } from '../../search/search';
+import { ClassTeacher } from "../../Teacher/class-teacher/class-teacher";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-dashboard-home',
     standalone: true,
-    imports: [CommonModule, Repository, Assets, KeyboardNav, Speak, Search],
+    imports: [CommonModule, Repository, Assets, KeyboardNav, Speak, Search, ClassTeacher,FormsModule],
     templateUrl: './dashboard-home.html',
 })
 export class DashboardHome {
-    private accountService = inject(AccountService);
+    public accountService = inject(AccountService);
 
     selectedClassId = signal<string | null>(null);
     selectedRepositoryId = signal<string | null>(null);
+    userRole = computed(() => this.accountService.currentUser()?.user.role);
+
+    isSidebarOpen: WritableSignal<boolean> = signal(false);
+    isNavOverlayActive = signal(false);
 
     studentClasses = computed<Class[]>(() => {
         const user = this.accountService.currentUser();
         return user?.student?.classes || [];
     });
 
-    // When class is clicked
     viewClass(id: string) {
         this.selectedClassId.set(id);
+        this.isSidebarOpen.set(false);
     }
 
-    // Repository clicked
     openRepository(repoId: string) {
         this.selectedRepositoryId.set(repoId);
     }
 
-    // Back from assets → go to repository list
     backFromAssets() {
         this.selectedRepositoryId.set(null);
     }
 
-    // Back from repositories → go to class list
     backToDashboard() {
         this.selectedClassId.set(null);
     }
+
+
+
 }
